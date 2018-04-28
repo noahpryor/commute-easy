7d30b3826c01f493804c47959e77bb93610b48af
Noah Pryor <noah@noahpryor.com>
2018-04-27 20:57:52 -0400

    Update icons script

--- create-icons.sh
+++ create-icons.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-convert logo.svg -resize 128 -format png -transparent white src/icons/icon-128.png
-convert logo.svg -resize 16 -format png -transparent white src/icons/icon-16.png
-convert logo.svg -resize 19 -format png -transparent white src/icons/icon-19.png
-convert logo.svg -resize 38 -format png -transparent white src/icons/icon-38.png
-convert logo.svg -resize 64 -format png -transparent white src/icons/icon-64.png
-convert logo.svg -resize 32 -format png -transparent white src/icons/icon-32.png
+convert logo.svg -resize 128 -format png -transparent white app/images/icon-128.png
+convert logo.svg -resize 16 -format png -transparent white app/images/icon-16.png
+convert logo.svg -resize 19 -format png -transparent white app/images/icon-19.png
+convert logo.svg -resize 38 -format png -transparent white app/images/icon-38.png
+convert logo.svg -resize 64 -format png -transparent white app/images/icon-64.png
+convert logo.svg -resize 32 -format png -transparent white app/images/icon-32.png
