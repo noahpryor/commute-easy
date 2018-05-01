12721b9e5e8325359f21af24961406172da02e25
Noah Pryor <noah@noahpryor.com>
2018-04-30 23:42:44 -0400

    Add chrome extension deploy script

---
+++ deploy.js
@@ -0,0 +1,58 @@
+require('dotenv').config()
+
+const chromeWebstoreUpload = require('chrome-webstore-upload')
+const {readJson} = require("fs-extra")
+const {resolve} = require("path")
+const {createReadStream} = require("fs")
+
+const webStore = chromeWebstoreUpload({
+    extensionId: process.env.CHROME_EXTENSION_ID,
+    clientId: process.env.CHROME_CLIENT_ID,
+    clientSecret: process.env.CHROME_CLIENT_SECRET,
+    refreshToken: process.env.CHROME_REFRESH_TOKEN
+});
+
+
+const UPLOAD_FAILURE_STATES = ["FAILURE", "NOT_FOUND"]
+/// response: https://developer.chrome.com/webstore/webstore_api/items#resource
+const uploadPackage = (packagePath, token) => {
+  return webStore.uploadExisting(createReadStream(packagePath), token)
+    .then(response => {
+      const { uploadState } = response;
+      // this API never rejects requests
+      if(UPLOAD_FAILURE_STATES.includes(uploadState)) {
+        const [{error_code, error_detail}] = response.itemError
+        const error = new Error(error_detail)
+        error.response = response
+        error.code = error_code
+        return Promise.reject(error)
+      }
+      return response
+    })
+}
+
+const uploadChromeExtension = async (target="default") => {
+  try {
+
+    //  Read file name and version from package.json
+    const {name, version} = await readJson("package.json")
+    const browser = "chrome";
+    const packagePath = resolve("packages", `${name}.v${version}.${browser}.zip`)
+
+    const token = await webStore.fetchToken()
+
+    const uploadResponse = await uploadPackage(packagePath, token)
+
+    const publishResponse = await webStore.publish(target, token)
+
+    console.log("Package published", publishResponse)
+
+  }
+  catch(e) {
+    console.error("Deploy failed!", e)
+    throw e
+  }
+  return {}
+}
+
+uploadChromeExtension()
